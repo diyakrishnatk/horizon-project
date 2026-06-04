4c2b45d9773e56fb641f97651fd7d11c0e594180
Diyakrishna <diyakrishnatk@gmail.com>
2026-06-04 14:30:24 +0530

    add tours

--- frontend/src/components/pages/Tours.jsx
+++ frontend/src/components/pages/Tours.jsx
@@ -13,17 +13,23 @@ const Tours = () => {
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(0);
 
-  const {data:tours,loading,error}=useFetch(`${BASE_URL}/tours?page=${page}`)
-  const {data:tourCount} = useFetch(`${BASE_URL}/tours/search/getTourCount`)
+  const { data: tours, loading, error } = useFetch(
+    `${BASE_URL}/tours?page=${page}`
+  );
 
+  console.log("Tours received:", tours);
+
+  const { data: tourCount } = useFetch(
+    `${BASE_URL}/tours/search/getTourCount`
+  );
 
   useEffect(() => {
     const pages = Math.ceil(tourCount / 8);
     setPageCount(pages);
-    window.scrollTo(0,0);
-  }, [page,tourCount,tours]);
+    window.scrollTo(0, 0);
+  }, [page, tourCount, tours]);
 
-
+  
   return (
     <>
       <CommonSection title={"All Tours"} />
--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,4 @@
 // To run locally use local host, for deployment use backend url
 // export const BASE_URL= 'http://localhost:4000/api/v1';  
-export const BASE_URL= 'https://horizon-project-pekz.onrender.com/api/v1';
+// To run locally use local host, for deployment use backend url
+export const BASE_URL = 'http://localhost:8000/api/v1';
